78e42f2375eb7d8813b52033568b2736d2397ca9
Adam <ats92@cox.net>
2021-12-29 09:05:16 -0800

    add and update functionality

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -7,7 +7,7 @@ type User {
     friendCount: Int
     thoughts: [Thought]
     friends: [User]
-  }
+}
 type Thought {
     _id: ID
     thoughtText: String
@@ -15,19 +15,32 @@ type Thought {
     username: String
     reactionCount: Int
     reactions: [Reaction]
-  }
-  type Reaction {
-      _id: ID
-      reactionBody: String
-      createdAt: String
-      username: String
-  }
-    type Query {
-        users: [User]
-        user(username: String!): User
-        thoughts(username: String): [Thought]
-        thought(_id: ID!): Thought
-    }
+}
+type Reaction {
+    _id: ID
+    reactionBody: String
+    createdAt: String
+    username: String
+}
+type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    thoughts(username: String): [Thought]
+    thought(_id: ID!): Thought
+}
+type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    addThought(thoughtText: String!): Thought
+    addReaction(thoughtId: ID!, reactionBody: String!): Thought
+    addFriend(friendId: ID!): User
+}
+type Auth {
+    token: ID!
+    user: User
+}
+
 `;
 
 
